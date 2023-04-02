69492d8d5686b46d38fa6174de466dffadd798c1
muhammmad shahzaib <shahzaibxmarty02@gmail.com>
2023-04-02 13:35:28 +0500

    this is a java script

---
+++ app.js
@@ -0,0 +1,17 @@
+// alert("HELLO WORLD");
+
+var text = "Lorem, ipsum dolor sit amet consectetur adipisicing elit Lorem, ipsum dolor sit amet consectetur adipisicing elit"
+// for (var i = 0; i < text.length; i++) {
+//     if (text.slice(i, i+8) = "sit amet") {
+//         text = text.slice(0, i) + "sit amet" + text.slice(i + 8);
+//     }
+    
+// }
+// console.log(text)
+
+// var sitamet= text.indexOf("sit amet")
+// console.log(sitamet);
+
+var sitamet= text.lastIndexOf("sit amet")
+console.log(sitamet);
+
